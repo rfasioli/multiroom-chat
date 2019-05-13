413575e72e7f256adce1847f94821fca8bda7202
Rodrigo Fasioli <rodrigo.fasioli@gmail.com>
2019-05-13 00:35:23 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+/* importar as configurações do servidor */
+var app = require('./config/server');
+
+/* parametrizar porta de escuta */
+var server = app.listen(3000, function(){
+    console.log('Servidor online');
+});
+
+var io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+/* Criar a conexão por WebSocket */
+io.on('connection', (socket) => {
+    console.log('Usuário conectou');
+
+    socket.on('disconnect', () => {
+        console.log('Usuário desconectou');
+    });
+
+    socket.on('msgParaServidor', (data) => {
+        /*dialogo*/
+        socket.emit(
+            'msgParaCliente', 
+            { apelido: data.apelido, mensagem: data.mensagem }
+        );
+        socket.broadcast.emit(
+            'msgParaCliente', 
+            { apelido: data.apelido, mensagem: data.mensagem }
+        );
+
+        /* participantes */
+        if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+            socket.emit(
+                'participantesParaCliente', 
+                { apelido: data.apelido}
+            );
+            socket.broadcast.emit(
+                'participantesParaCliente', 
+                { apelido: data.apelido}
+            );
+        }
+    });
+});
